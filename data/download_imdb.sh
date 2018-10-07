5951e7a1096c34e4ec9d2152dd02663b8bad4746
Volkan Cirik <vcirik@cs.cmu.edu>
2018-10-07 18:03:57 -0400

    blacklist.txt is added.

--- data/download_imdb.sh
+++ data/download_imdb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget http://people.eecs.berkeley.edu/~ronghang/projects/cmn/data/exp-refgoog/imdb.tgz
-wge https://www.dropbox.com/s/6rlkbq6ggfj0nqy/blacklist.txt
+wget https://www.dropbox.com/s/6rlkbq6ggfj0nqy/blacklist.txt
 tar xvf imdb.tgz
