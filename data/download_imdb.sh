7b385c92f4c2c8b06e989c1d27f0853e2a160bac
volkan <1189783+volkancirik@users.noreply.github.com>
2020-10-07 14:06:31 -0400

    Update download_imdb.sh

--- data/download_imdb.sh
+++ data/download_imdb.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 wget http://people.eecs.berkeley.edu/~ronghang/projects/cmn/data/exp-refgoog/imdb.tgz
-wget https://www.dropbox.com/s/6rlkbq6ggfj0nqy/blacklist.txt
+
+wget https://cmu.box.com/shared/static/kz5ptak0km2fsvqet0dryz80aisc3u0a.txt
+
 tar xvf imdb.tgz
