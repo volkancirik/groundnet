315f18a30a79278f9c415af687140d90af54953a
volkan <1189783+volkancirik@users.noreply.github.com>
2020-10-07 14:05:38 -0400

    Update download_coco.sh

--- data/download_coco.sh
+++ data/download_coco.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-wget https://www.dropbox.com/s/q53f27g75pxzgcn/google_refexp_train_201511_coco_aligned.json
-wget https://www.dropbox.com/s/4wvw7375o9x86e3/google_refexp_val_201511_coco_aligned.json
 
-wget https://www.dropbox.com/s/nnlbszkus8cyel8/instances_train2014.json
-wget https://www.dropbox.com/s/7957mw5z5wlz8g8/instances_val2014.json
+wget https://cmu.box.com/shared/static/hrk3g9dxc279hi8lcb7wjy0cayqj3bok.json
+wget https://cmu.box.com/shared/static/2gtk8smfhheidix6szhpytglrx0y74tv.json
 
-wget https://www.dropbox.com/s/o5ijoaxcymtsf24/google_refexp_val_201511_coco_aligned_mcg_umd.json
-wget https://www.dropbox.com/s/0jrdbg2svmvdmgl/google_refexp_train_201511_coco_aligned_mcg_umd.json
+wget https://cmu.box.com/shared/static/o0o8btuzb4xqk8sgy7mvslexxutc82us.json
+wget https://cmu.box.com/shared/static/ofyqypokehn7dyqh93jf6nzthssg5vp0.json
+
+wget https://cmu.box.com/shared/static/c8lshs8tbfgkvrc4fh7gvk827p3s1mzl.json
+wget https://cmu.box.com/shared/static/2fuqr1mryfrt4urvkh2bz3sq1cw3ch07.json
